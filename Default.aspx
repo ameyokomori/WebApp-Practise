02b5757cd9d7a97a687f994f18e908350a1ac118
waynezhao12 <zhaoweiye1993@gmail.com>
2017-12-09 00:30:50 +1000

    1

---
+++ Default.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="Welcome to prac1."></asp:Label>
+    
+    </div>
+        <p>
+            <asp:Button ID="Btn_Calculate" runat="server" OnClick="Button1_Click" Text="Calculate" />
+&nbsp;
+            <asp:TextBox ID="Tb_Input_1" runat="server">0</asp:TextBox>
+&nbsp;
+            <asp:DropDownList ID="DropDownList1" runat="server">
+                <asp:ListItem>+</asp:ListItem>
+                <asp:ListItem>-</asp:ListItem>
+                <asp:ListItem>*</asp:ListItem>
+                <asp:ListItem>/</asp:ListItem>
+            </asp:DropDownList>
+&nbsp;
+            <asp:TextBox ID="Tb_Input_2" runat="server">0</asp:TextBox>
+&nbsp;
+            <asp:Label ID="Label2" runat="server" Text="=Base10:"></asp:Label>
+&nbsp;<asp:TextBox ID="Tb_Output_B10" runat="server" ReadOnly="True">0</asp:TextBox>
+&nbsp;
+            <asp:Label ID="Label3" runat="server" Text="=Base2:"></asp:Label>
+&nbsp;<asp:TextBox ID="Tb_Output_B2" runat="server" ReadOnly="True">0</asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="Btn_Count" runat="server" OnClick="Button2_Click" Text="Count" />
+        </p>
+        <p>
+            <asp:Label ID="Label4" runat="server" Text="Num of 0"></asp:Label>
+&nbsp;<asp:TextBox ID="Tb_Output_Num0" runat="server" ReadOnly="True">0</asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label5" runat="server" Text="Num of 1"></asp:Label>
+&nbsp;<asp:TextBox ID="Tb_Output_Num1" runat="server" ReadOnly="True">0</asp:TextBox>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        double result = 0;
+        double input1 = 0;
+        double input2 = 0;
+
+        Tb_Output_B10.Text = "0";
+        Tb_Output_B2.Text = "0";
+        Tb_Output_Num0.Text = "0";
+        Tb_Output_Num1.Text = "0";
+
+        try
+        {
+            input1 = Convert.ToDouble(Tb_Input_1.Text);
+            input2 = Convert.ToDouble(Tb_Input_2.Text);
+        }
+        catch (FormatException fe)
+        {
+            Console.WriteLine("Exception caught: {0}", fe);
+            Tb_Input_1.Text = "Wrong input!";
+            Tb_Input_2.Text = "Try again!";
+        }
+
+        if (DropDownList1.SelectedValue.Equals("+"))
+        {
+            result = input1 + input2;
+        }
+        else if (DropDownList1.SelectedValue.Equals("-"))
+        {
+            result = input1 - input2;
+        }
+        else if (DropDownList1.SelectedValue.Equals("*"))
+        {
+            result = input1 * input2;
+        }
+        else if (DropDownList1.SelectedValue.Equals("/"))
+        {
+            result = input1 / input2;
+        }
+
+        Tb_Output_B10.Text = result.ToString();
+        Tb_Output_B2.Text = CalculateBase2(result);
+
+    }
+
+    protected String CalculateBase2(double input)
+    {
+        int integerNum = (int)input;
+        double doubleNum = Math.Abs(input - integerNum);
+
+        if (doubleNum == 0)
+        {
+            String base2Int = CalculateIntBase2(integerNum);
+            return base2Int;
+        }
+        else
+        {
+            String base2Int = CalculateIntBase2(integerNum);
+            String base2Double = CalculateFloatBase2(doubleNum);
+            return base2Int + "." + base2Double;
+        }
+    }
+
+    protected String CalculateIntBase2(int input)
+    {
+        return Convert.ToString(input, 2);
+    }
+
+    protected String CalculateFloatBase2(double input)
+    {
+        input *= 2;
+
+        return (input == 1) ? "1" : (((int)input).ToString() + CalculateFloatBase2(input - (int)input));
+    }
+    
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Tb_Output_Num0.Text = (Tb_Output_B2.Text.Split('0').Length - 1).ToString();
+        Tb_Output_Num1.Text = (Tb_Output_B2.Text.Split('1').Length - 1).ToString();
+    }
+}
